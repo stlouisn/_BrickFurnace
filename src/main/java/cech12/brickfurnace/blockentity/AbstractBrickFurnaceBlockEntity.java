20855f284ba11a996ec0fcf3460e62adf4e14889
cech12 <cech12@gmail.com>
2023-02-06 17:24:37 +0100

    reduce complexity of tick AbstractBrickFurnaceBlockEntity::tick (Codefactor hint)

--- src/main/java/cech12/brickfurnace/blockentity/AbstractBrickFurnaceBlockEntity.java
+++ src/main/java/cech12/brickfurnace/blockentity/AbstractBrickFurnaceBlockEntity.java
@@ -60,51 +60,52 @@ public abstract class AbstractBrickFurnaceBlockEntity extends AbstractFurnaceBlo
             entity.dataAccess.set(BURN_TIME, entity.dataAccess.get(BURN_TIME) - 1); //changed because of private variable
         }
 
-        if (level != null && !level.isClientSide) {
-            ItemStack fuel = entity.items.get(FUEL);
-            if (entity.isBurning() || !fuel.isEmpty() && !entity.items.get(INPUT).isEmpty()) {
-                AbstractCookingRecipe irecipe = entity.getRecipe();
-                boolean valid = entity.canBurn(irecipe);
-                if (!entity.isBurning() && valid) {
-                    entity.dataAccess.set(BURN_TIME, entity.getBurnDuration(fuel)); //changed because of private variable
-                    entity.dataAccess.set(RECIPES_USED, entity.dataAccess.get(BURN_TIME)); //changed because of private variable
-                    if (entity.isBurning()) {
-                        dirty = true;
-                        if (fuel.hasCraftingRemainingItem()) entity.items.set(1, fuel.getCraftingRemainingItem());
-                        else if (!fuel.isEmpty()) {
-                            fuel.shrink(1);
-                            if (fuel.isEmpty()) {
-                                entity.items.set(1, fuel.getCraftingRemainingItem());
-                            }
+        if (level == null || level.isClientSide) {
+            return;
+        }
+
+        ItemStack fuel = entity.items.get(FUEL);
+        if (entity.isBurning() || !fuel.isEmpty() && !entity.items.get(INPUT).isEmpty()) {
+            AbstractCookingRecipe irecipe = entity.getRecipe();
+            boolean valid = entity.canBurn(irecipe);
+            if (!entity.isBurning() && valid) {
+                entity.dataAccess.set(BURN_TIME, entity.getBurnDuration(fuel)); //changed because of private variable
+                entity.dataAccess.set(RECIPES_USED, entity.dataAccess.get(BURN_TIME)); //changed because of private variable
+                if (entity.isBurning()) {
+                    dirty = true;
+                    if (fuel.hasCraftingRemainingItem()) entity.items.set(1, fuel.getCraftingRemainingItem());
+                    else if (!fuel.isEmpty()) {
+                        fuel.shrink(1);
+                        if (fuel.isEmpty()) {
+                            entity.items.set(1, fuel.getCraftingRemainingItem());
                         }
                     }
                 }
+            }
 
-                if (entity.isBurning() && valid) {
-                    entity.dataAccess.set(COOK_TIME, entity.dataAccess.get(COOK_TIME) + 1); //changed because of private variable
-                    if (entity.dataAccess.get(COOK_TIME) == entity.dataAccess.get(COOK_TIME_TOTAL)) { //changed because of private variable
-                        entity.dataAccess.set(COOK_TIME, 0); //changed because of private variable
-                        entity.dataAccess.set(COOK_TIME_TOTAL, entity.getTotalCookTime()); //changed because of private variable
-                        entity.smeltItem(irecipe);
-                        dirty = true;
-                    }
-                } else {
+            if (entity.isBurning() && valid) {
+                entity.dataAccess.set(COOK_TIME, entity.dataAccess.get(COOK_TIME) + 1); //changed because of private variable
+                if (entity.dataAccess.get(COOK_TIME) == entity.dataAccess.get(COOK_TIME_TOTAL)) { //changed because of private variable
                     entity.dataAccess.set(COOK_TIME, 0); //changed because of private variable
+                    entity.dataAccess.set(COOK_TIME_TOTAL, entity.getTotalCookTime()); //changed because of private variable
+                    entity.smeltItem(irecipe);
+                    dirty = true;
                 }
-            } else if (!entity.isBurning() && entity.dataAccess.get(COOK_TIME) > 0) { //changed because of private variable
-                entity.dataAccess.set(COOK_TIME, Mth.clamp(entity.dataAccess.get(COOK_TIME) - 2, 0, entity.dataAccess.get(COOK_TIME_TOTAL))); //changed because of private variable
+            } else {
+                entity.dataAccess.set(COOK_TIME, 0); //changed because of private variable
             }
+        } else if (!entity.isBurning() && entity.dataAccess.get(COOK_TIME) > 0) { //changed because of private variable
+            entity.dataAccess.set(COOK_TIME, Mth.clamp(entity.dataAccess.get(COOK_TIME) - 2, 0, entity.dataAccess.get(COOK_TIME_TOTAL))); //changed because of private variable
+        }
 
-            if (wasBurning != entity.isBurning()) {
-                dirty = true;
-                level.setBlock(pos, state.setValue(AbstractFurnaceBlock.LIT, entity.isBurning()), 3);
-            }
+        if (wasBurning != entity.isBurning()) {
+            dirty = true;
+            level.setBlock(pos, state.setValue(AbstractFurnaceBlock.LIT, entity.isBurning()), 3);
         }
 
         if (dirty) {
             entity.setChanged();
         }
-
     }
 
     private boolean canBurn(@Nullable Recipe<?> recipe) {
